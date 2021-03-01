6b981f353a8ad505c4d90395a0b9bcac26b12d03
Oil Sudarat <sudarat@enres.co>
2021-03-01 16:46:28 +0700

    26. On Menu Click Functionality

--- todos/index.js
+++ todos/index.js
@@ -15,6 +15,18 @@ app.on("ready", () => {
 const menuTemplate = [
   {
     label: "File",
-    submenu: [{ label: "New Todo" }],
+    submenu: [
+      { label: "New Todo" },
+      {
+        label: "Quit",
+        click() {
+          app.quit();
+        },
+      },
+    ],
   },
 ];
+
+if (process.platform === "darwin") {
+  menuTemplate.unshift({ label: "" });
+}
